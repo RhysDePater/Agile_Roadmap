f69ef5ccd2590824930e6a536d686ca7919fb12a
RhysDePater <rhys.depater@hotmail.com>
2021-05-17 11:49:18 +1000

    pushing code to repo

---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+import ForgeUI, { render, ProjectPage, Fragment, Text, useProductContext, useState } from '@forge/ui';
+import React from 'react';
+import api from "@forge/api";
+import { fetch } from '@forge/api';
+
+const fetchCommentsForIssue = async () => {
+    const res = await api
+      .asApp()
+      .requestJira(`/rest/api/3/issue/CKRS-29`);
+  
+    const data = await res.json();
+    return data;
+  };
+  
+      
+const App = () => {
+    const context = useProductContext();
+    const [comments] = useState(async () => await fetchCommentsForIssue());
+    console.log("TEST")
+
+    return (
+        <Fragment>
+            <Text>Hello world 4!</Text>
+            <Text>{JSON.stringify(comments)}</Text>
+        </Fragment>
+    );
+};
+
+export const run = render(
+    <ProjectPage>
+        <App />
+    </ProjectPage>
+);
