95b5a4215e32958a0494a6cd859a6a44172c6788
jfanc344 <32828802+jfanc344@users.noreply.github.com>
2021-08-03 14:58:49 +1000

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,14 @@
-import ForgeUI, {
-  render,
-  ProjectPage,
-  Fragment,
-  Text,
-  useProductContext,
-  useState,
-} from "@forge/ui";
-import React from "react";
-import api from "@forge/api";
-import { fetch } from '@forge/api';
+import Resolver from '@forge/resolver';
 
-export async function fetchCommentsForIssue() {
-  const res = await api
-      .asApp()
-      .requestJira(`/rest/api/3/issue/CKRS-29`);
-  
-    const data = await res.json();
-    return data;
-  };
-  
-      
-const App = () => {
-  const context = useProductContext();
-  const [comments] = useState(async () => await fetchCommentsForIssue());
-  console.log("TEST");
+const resolver = new Resolver();
 
-  return (
-    <Fragment>
-      <Text> Agile Roadmap world 4! </Text> <Text> {JSON.stringify(comments)} </Text>{" "}
-    </Fragment>
-  );
-};
+resolver.define('getText', (req) => {
+    console.log(req);
 
-export const run = render(
-  <ProjectPage>
-    <App />
-  </ProjectPage>
-);
+    return (
+        <div><h1>HELOO</h1></div>
+    );
+});
+
+export const handler = resolver.getDefinitions();
+
---
+++ static/hello-world/src/App.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Container,
+    Row,
+    Col,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    InputGroup,
+    InputGroupAddon
+} from "reactstrap";
+
+import "./App.css"
+
+function Column(props) {
+    return (
+        <Col class="col">
+            <div class="columnBox"> {props.title}</div>
+        </Col>
+    );
+}
+
+function App() {
+
+    const [ColumnName, SetColumnName] = useState();
+    const [columns, setColumns] = useState([
+        { id: 1, title: "Column 1" },
+        { id: 1, title: "Column 2" }
+    ]);
+
+    function handleNewColumn() {
+        // it's important to not mutate state directly, so here we are creating a copy of the current state using the spread syntax
+        const updateUsers = [
+            // copy the current users state
+            ...columns,
+            // now you can add a new object to add to the array
+            {
+                // using the length of the array for a unique id
+                id: columns.length + 1,
+                // adding a new user name
+                title: ColumnName
+            }
+        ];
+        // update the state to the updatedUsers
+        setColumns(updateUsers);
+    }
+
+
+    return (
+        <div>
+            
+            <Container fluid={true}>
+                <Row xs="5">
+                    <Form
+                        inline
+                        onSubmit={() => {
+                            event.preventDefault();
+                            handleNewColumn();
+                        }}
+                    >
+                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                            <InputGroup>
+                                <Input
+                                    type="text"
+                                    id="newColumnInput"
+                                    value={ColumnName}
+                                    onChange={(event) => {
+                                        const { value } = event.target;
+                                        SetColumnName(value);
+                                    }}
+                                />
+                                <InputGroupAddon addonType="append">
+                                    <Button color="secondary" type="submit">
+                                        Add Column
+                                    </Button>
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </FormGroup>
+                    </Form>
+                </Row>
+                <br />
+
+                <Row xs="6">
+                <Col class="col">
+                    <div class="columnBox"> Column <div class="card"> card 1 </div></div>
+                    </Col>
+                    {columns.map((column) => (
+                        <Column title={column.title} />
+                    ))}
+                    
+                    
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
